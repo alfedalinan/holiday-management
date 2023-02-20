2fe1749464845a2fc9577e7468d14fa77dbeaa46
Alfed Alinan <alfed.alinan@growsari.com>
2023-02-21 03:16:28 +0800

    Removed duplicate of user creation

--- src/infrastructure/routes/index.js
+++ src/infrastructure/routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const userRoutes = require('./user');
 const holidayRoutes = require('./holiday');
 const loginRoutes = require('./login');
 const signUpRoutes = require('./sign-up');
 const userHolidayRoutes = require('./user-holiday');
 
-router.use('/user', userRoutes.router);
 router.use('/holiday', holidayRoutes.router);
 router.use('/login', loginRoutes.router);
 router.use('/sign-up', signUpRoutes.router);
